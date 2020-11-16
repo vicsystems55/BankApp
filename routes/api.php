415e1cb088f750692ecd26203ed9c40b3176014e
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-11-16 23:12:59 +0100

    upload api

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+// header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
 
 Route::post('login', 'Api\AuthController@login');
 
