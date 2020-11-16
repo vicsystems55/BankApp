265787974251be3ecd6bf9e233f7f1266185109e
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-11-16 22:42:48 +0100

    upload api

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'rave/callback'
+        'rave/callback',
+        'upload_pix',
+        
     ];
 }
