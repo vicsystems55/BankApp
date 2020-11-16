f5ee35056fc9e5f4f275a6c31884d4cac17c02ab
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2020-11-16 21:58:59 +0100

    upload api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\ApifileUpload;
+use App\ApiFileUpload;
 
 class PostController extends Controller
 {
